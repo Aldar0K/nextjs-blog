4fc72d90dda83a36cadc784993012054943bf5c7
Aldar <aldar@MacBook-Pro-Aldar.local>
2023-09-08 11:20:13 +0300

    feat: add credentials auth

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,11 +1,44 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, User } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+
+// TODO move to models
+export type IUser = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+// TODO move to db
+export const users: IUser[] = [
+  { email: "aldarokonov@gmail.com", password: "12345678", name: "Aldar" },
+];
 
 export const authConfig: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Credentials({
+      credentials: {
+        email: { label: "email", type: "email", required: true },
+        password: { label: "password", type: "password", required: true },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const currentUser = users.find(
+          (user) => user.email === credentials.email
+        );
+
+        if (currentUser && currentUser.password === credentials.password) {
+          const { password, ...userWithoutPassword } = currentUser;
+          return userWithoutPassword as User;
+        }
+
+        return null;
+      },
     }),
   ],
 };
