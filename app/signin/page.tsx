20be2357da003f71267c3f29360180cc25bc5295
Aldar <aldar@MacBook-Pro-Aldar.local>
2023-09-08 11:38:49 +0300

    feat: add custom cred signin form

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,12 @@
-import { GoogleButton } from "components";
+import { GoogleButton, SignInForm } from "components";
 
 const Signin = async () => {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h1>Sign In</h1>
       <GoogleButton />
+      <div>or</div>
+      <SignInForm />
     </div>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export * from "./goggle-button";
 export * from "./header";
 export * from "./navigation";
 export * from "./providers";
+export * from "./signin-form";
