f7c32373c2381c5c5f341c23b5d533fd27a9ac02
Aldar <aldar@MacBook-Pro-Aldar.local>
2023-09-12 19:28:43 +0300

    feat: update getSortedPostsData method

--- lib/posts/getSortedPostsData.ts
+++ lib/posts/getSortedPostsData.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fsp from "fs/promises";
 import matter from "gray-matter";
 import path from "path";
 
@@ -12,30 +12,36 @@ export const getSortedPostsData = async (): Promise<
   }[]
 > => {
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, "");
+  const fileNames = await fsp.readdir(postsDirectory);
+  const allPostsData = await Promise.all(
+    fileNames.map(async (fileName) => {
+      // Remove ".md" from file name to get id
+      const id = fileName.replace(/\.md$/, "");
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+      // Read markdown file as string
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = await fsp.readFile(fullPath, "utf8");
 
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+      // Use gray-matter to parse the post metadata section
+      const matterResult = matter(fileContents);
 
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as { date: string; title: string }),
-    };
-  });
+      // Combine the data with the id
+      return {
+        id,
+        ...(matterResult.data as { date: string; title: string }),
+      };
+    })
+  );
+
   // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
+  const sortedPostsData = allPostsData.sort((a, b) => {
+    // Convert date strings to Date objects for comparison
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+
+    // Sort in descending order (latest date first)
+    return dateB.getTime() - dateA.getTime();
   });
+
+  return sortedPostsData;
 };
